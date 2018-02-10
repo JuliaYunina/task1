db7bf09653f00b9393e9c71275030f5bff2675be
Julia Yunina <kontakt_1@inbox.ru>
2018-02-10 16:30:53 +0300

    package Calculator - Exception Max cout word

--- src/main/java/Calculator/Delenie.java
+++ src/main/java/Calculator/Delenie.java
@@ -1,17 +1,23 @@
 package Calculator;
 
-public class Delenie implements Calculate{
+public class Delenie implements Calculate {
     float arg1;
     float arg2;
-
-    Delenie(float arg1, float arg2) {
+     Delenie(float arg1, float arg2) {
         this.arg1 = arg1;
         this.arg2 = arg2;
     }
     @Override
-    public void calcOper() {
+        public void calcOper () {
         float delen = 0;
-        delen = arg1 / arg2;
+//        try {
+            delen = arg1 / arg2;
+ /*           if (arg2 == 0)
+            {throw new ArithmeticException();}
+        }
+        catch (ArithmeticException err1) {
+            System.out.println("Деление на 0\n");
+        }*/
         System.out.printf("Частное от деления числа %f на число %f равно: %.4f", arg1, arg2, delen);
     }
 }
---
+++ src/main/java/CountWords/CountWords.java
@@ -0,0 +1,47 @@
+package CountWords;
+import java.io.File;
+import java.util.*;
+
+public class CountWords {
+    public static void main(String[] args) {
+        try {
+            Scanner scanner = new Scanner(new File("D:\\tekst.txt"),"windows-1251");
+            Set<String> words = new TreeSet<>();
+            Map<String, Integer> statistics = new HashMap<>();
+            String maxCountW = "";
+            int maxCount = 0;
+            while (scanner.hasNext()) {
+                String word = scanner.useDelimiter("\\s+").next();
+                words.add(word);
+                Integer countw = statistics.get(word);
+                if (countw == null) {
+                    countw = 0;
+                }
+                statistics.put(word, ++countw);
+                if (maxCount<countw){
+                    maxCount = countw;
+                    maxCountW = word;
+                }
+            }
+            System.out.println(words);
+            System.out.println(statistics);
+            System.out.println("Максимальное число повторений: " + maxCount + " у слова " + maxCountW);
+        } catch (Exception exception) {
+            System.out.println("File not found");
+        }
+    }
+}
+
+//121312
+/*String[] m = "abc abcde abcdef words abc abc g".split(" ");
+    HashMap<String, Integer> statistics = new HashMap<String, Integer>();
+
+        String s = null;
+        int p = 0;
+        for (String w : h.keySet()) {
+        if (p < h.get(w)) {
+        p = h.get(w);
+        s = w;
+        }
+
+        System.out.println("Самое повторяющееся слово: " + w + " (встречается " + h.get(w)*/
